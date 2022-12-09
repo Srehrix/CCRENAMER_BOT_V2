0be8a25d559a25e866839dc71e21686b1318b287
Sree Hari <98310399+Srehrix@users.noreply.github.com>
2022-12-09 11:40:12 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -42,8 +42,8 @@ async def start(client,message):
 	**Document Or Video** and enter new filename to rename it__
 	""",reply_to_message_id = message.id ,  
 	reply_markup=InlineKeyboardMarkup(
-	 [[ InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/lntechnical") ], 
-	[InlineKeyboardButton("Subscribe 🧐", url="https://youtube.com/c/LNtechnical") ]  ]))
+	 [[ InlineKeyboardButton("Updates" ,url="https://t.me/Tiyaan_bots") ], 
+	[InlineKeyboardButton("Official", url="https://t.me/CinemaCompanyofficials") ]  ]))
 	    return
 	if id:
 	    if old == True:
@@ -55,8 +55,8 @@ async def start(client,message):
 	**Document Or Video** and enter new filename to rename it__
 	""",reply_to_message_id = message.id ,  
 	reply_markup=InlineKeyboardMarkup(
-	 [[ InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/lntechnical") ], 
-	[InlineKeyboardButton("Subscribe 🧐", url="https://youtube.com/c/LNtechnical") ]  ]))
+	 [[ InlineKeyboardButton("Updates" ,url="https://t.me/Tiyaan_bots") ], 
+	[InlineKeyboardButton("Official", url="https://t.me/CinemaCompanyofficials") ]  ]))
 	        except:
 	             return
 	    else:
